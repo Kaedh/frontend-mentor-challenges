a41f050f27ea30f637d8bc1021a646aa5a03b8bf
leonardo martins <leonardomds2013@gmail.com>
2021-01-19 19:51:15 -0300

    :sparkles: Article Preview - Desktop version finished

--- src/article-preview/js/main.js
+++ src/article-preview/js/main.js
@@ -2,4 +2,5 @@ const shareBtn = document.getElementById('share-btn');
 
 shareBtn.addEventListener('click', () => {
     document.getElementById('hidden-menu').classList.toggle('visible')
+    document.querySelector('.share-box').classList.toggle('show-arrow')
 });
