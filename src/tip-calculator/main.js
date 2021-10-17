5bed4185e3e4e5e897f1fd9f08abbe2c303961a3
kaedh <leonardomds2013@gmail.com>
2021-10-16 21:04:31 -0300

    :sparkles: tip-calculator - minor fix in design and initial js

---
+++ src/tip-calculator/main.js
@@ -0,0 +1,8 @@
+// É preciso que quando clica no container do input abre a edicao
+// Salvar valorTotalDaConta
+// Salvar porcentagem de gorjeta
+// Se for custom é preciso ajustar css aqui para que quando clique zera o valor e deixe com a cor de selecionado
+// salvar o numero de pessoas
+// assim que as 3 informações forem escritas, mostrar na tela da direita o valor calculado do total de gorjeta
+//  e do total da conta pra cada pessoa.
+// Se o valor da quantidade de pessoas for 0 é preciso mostrar uma mensagem de alerta
