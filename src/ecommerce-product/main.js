ec6abedb0d8ac77379b5872b60814955df56c5a0
kaedh <leonardomds2013@gmail.com>
2021-11-02 15:35:05 -0300

    :sparkles: ecommerce-product - fix cart overlay position

--- src/ecommerce-product/main.js
+++ src/ecommerce-product/main.js
@@ -2,7 +2,7 @@ const cartButton = document.querySelector(".cart-button");
 const cartOverlay = document.querySelector(".cart-overlay")
 
 
+
 cartButton.addEventListener('click', () => {
   cartOverlay.classList.toggle('visible')
-  
 });
