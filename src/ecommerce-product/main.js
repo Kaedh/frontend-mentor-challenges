44506afe12ab200b82cd1adb4adafd9c3d3bc68b
kaedh <leonardomds2013@gmail.com>
2021-11-03 02:00:04 -0300

    :sparkles: ecommerce-product - carrousel css and js

--- src/ecommerce-product/main.js
+++ src/ecommerce-product/main.js
@@ -1,8 +1,21 @@
 const cartButton = document.querySelector(".cart-button");
-const cartOverlay = document.querySelector(".cart-overlay")
+const cartOverlay = document.querySelector(".cart-overlay");
+const carrousel = document.querySelector(".gallery-carrousel-container");
+const closeCarrousel = document.querySelector('.close-btn');
+const thumbImages = document.querySelectorAll(".thumb-container");
 
 
 
 cartButton.addEventListener('click', () => {
   cartOverlay.classList.toggle('visible')
 });
+
+thumbImages.forEach( thumb => {
+  thumb.addEventListener('click', () => {
+    carrousel.classList.toggle('visible')
+  }); 
+} )
+
+closeCarrousel.addEventListener('click', () => {
+  carrousel.classList = 'gallery-carrousel-container'
+})
