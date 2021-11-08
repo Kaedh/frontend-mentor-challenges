d7ee401c76c16900ac639ea906d5f911eb59efb4
kaedh <leonardomds2013@gmail.com>
2021-11-08 02:30:40 -0300

    :hammer: ecommerce-product - fix only when click on bigger image open carrousel

--- src/ecommerce-product/main.js
+++ src/ecommerce-product/main.js
@@ -1,13 +1,37 @@
+// cart 
 const cartButton = document.querySelector(".cart-button");
 const cartOverlay = document.querySelector(".cart-overlay");
+
+
+// carrousel
 const carrousel = document.querySelector(".gallery-carrousel-container");
 const closeCarrousel = document.querySelector('.close-btn');
 const thumbImages = document.querySelectorAll(".thumb-container");
+const selectImage = document.querySelector(".selected-image");
 
+// side menu
 const openMenuIcon = document.querySelector(".menu-icon");
 const closeSideMenuIcon = document.querySelector(".hidden-menu-close-btn");
 const navList = document.querySelector(".nav-list");
 const sideMenu = document.querySelector(".header-menu");
+
+// increase/decrease amount
+const amountLabel = document.querySelector(".amount");
+const increaseAmountButton = document.querySelector(".increase-amount");
+const decreaseAmountButton = document.querySelector(".decrease-amount");
+
+increaseAmountButton.addEventListener('click', () => {
+  const amountValueParsed = parseInt(amountLabel.textContent)
+  amountLabel.textContent = amountValueParsed + 1
+})
+
+decreaseAmountButton.addEventListener('click', () => {
+  const amountValueParsed = parseInt(amountLabel.textContent)
+
+  if (amountValueParsed > 0 ) amountLabel.textContent = amountValueParsed - 1
+
+  
+})
 
 openMenuIcon.addEventListener('click', () => {
   sideMenu.classList.toggle('visible')
@@ -24,24 +48,27 @@ cartButton.addEventListener('click', () => {
   cartOverlay.classList.toggle('visible')
 });
 
+
+selectImage.addEventListener('click', () => {
+  carrousel.classList.toggle('visible')
+})
+
+/*
 thumbImages.forEach( thumb => {
   thumb.addEventListener('click', () => {
     carrousel.classList.toggle('visible')
   }); 
-} )
+})
+*/
 
 closeCarrousel.addEventListener('click', () => {
   carrousel.classList = 'gallery-carrousel-container'
 })
 
-// Botoes de adicionar ou remover quantidade
 // Adicionar produto ao carrinho
 // carrinho vazio
-// menu lateral
 // ao clicar na thumb mudar a imagem
 // ao clicar na imagem grande aparecer o carrosel
 // ao clicar nas setas do carrosel retroceder ou avançar imagem
 // ao clicar na lixeira excluir do carrinho
-// quando clicar no menu hamburguer abrir o menu lateral
-// quando clicar no x do menu lateral fechar menu hamburguer
 // fechar carrosel apenas ao clicar no x
